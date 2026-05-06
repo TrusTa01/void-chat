88f785b7576d2321fe6d7f3018dac6503f94519f
Yuriy Borovikov <danylnet65@gmail.com>
2026-05-06 13:23:06 +1000

    add temporary buttons

--- frontend/lib/features/auth/presentation/screens/login_screen.dart
+++ frontend/lib/features/auth/presentation/screens/login_screen.dart
@@ -1,12 +1,59 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:void_chat/features/auth/presentation/screens/register_screen.dart';
 
 @RoutePage()
-class LoginScreen extends StatelessWidget {
+class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
 
   @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  final TextEditingController _controller = TextEditingController();
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(body: Center(child: Text('Login screen')));
+    return Scaffold(
+      appBar: AppBar(title: const Text('Register screen')),
+      body: Center(
+        child: Row(
+          children: [
+            ConstrainedBox(
+              constraints: const BoxConstraints(maxWidth: 150),
+              child: TextFormField(
+                decoration: const InputDecoration(labelText: 'Name'),
+                controller: _controller,
+              ),
+            ),
+
+            const SizedBox(height: 20),
+
+            TextButton(
+              onPressed: () {
+                debugPrint('Текст из поля: ${_controller.text}');
+              },
+              child: const Text('Войти'),
+            ),
+
+            const SizedBox(height: 20),
+
+            TextButton(
+              onPressed: () => Navigator.of(
+                context,
+              ).push(MaterialPageRoute(builder: (_) => const RegisterScreen())),
+              child: const Text('Зарегистрироваться'),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
--- frontend/lib/features/auth/presentation/screens/register_screen.dart
+++ frontend/lib/features/auth/presentation/screens/register_screen.dart
@@ -2,11 +2,51 @@ import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 
 @RoutePage()
-class RegisterScreen extends StatelessWidget {
+class RegisterScreen extends StatefulWidget {
   const RegisterScreen({super.key});
 
   @override
+  State<RegisterScreen> createState() => _RegisterScreenState();
+}
+
+class _RegisterScreenState extends State<RegisterScreen> {
+  final TextEditingController _controller = TextEditingController();
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(body: Center(child: Text('Register screen')));
+    return Scaffold(
+      appBar: AppBar(title: const Text('Register screen')),
+      body: Center(
+        child: Row(
+          children: [
+            ConstrainedBox(
+              constraints: const BoxConstraints(maxWidth: 150),
+              child: TextFormField(
+                decoration: const InputDecoration(labelText: 'Email'),
+                controller: _controller,
+              ),
+            ),
+
+            const SizedBox(height: 20),
+
+            TextButton(
+              onPressed: () {
+                debugPrint('Текст из поля: ${_controller.text}');
+              },
+              child: const Text('Войти'),
+            ),
+
+            const SizedBox(height: 20),
+
+            TextButton(
+              onPressed: () => Navigator.of(
+                context,
+              ).push(MaterialPageRoute(builder: (_) => const RegisterScreen())),
+              child: const Text('Зарегистрироваться'),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
